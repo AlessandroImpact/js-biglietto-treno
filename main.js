a1a48bba050d5c4451544afc3e8c2d46d921ca28
Alessandro Terracciano <terracciano41@gmail.com>
2022-01-20 13:18:56 +0100

    richiesta dati utente

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+alert("Benvenuto! Premi Invio per calcolare il prezzo del tuo biglietto");
+
+/*richiesta età */
+
+let eta = parseInt(prompt("Inserisci la tua età"));
+
+/*richiesta chilometraggio */
+
+let km = parseInt(prompt("Inserisci la distanza del tuo viaggio in chilometri"));
+
